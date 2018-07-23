75a1160182db64517445e7cb04f2f142ab12340b
cameron wiley <cswiley@gmail.com>
2018-07-23 09:21:10 -0700

    cms viewer update

--- resources/views/cms.blade.php
+++ resources/views/cms.blade.php
@@ -8,6 +8,6 @@
 
 @section('main')
 <div class="row column medium-10 small-12">
-    <cms-viewer path="{{ $title }}" get="{{ cms_api_path('test') }}" post="{{ cms_path() }}"></cms-viewer>
+    <cms-viewer path="{{ $title }}" get="{{ cms_api_path($title) }}" post="{{ cms_path() }}"></cms-viewer>
 </div>
 @stop
